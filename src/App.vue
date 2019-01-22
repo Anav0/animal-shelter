a702298d9cf84edb72debc28e46b363423a2bace
Anav <janbul6@gmail.com>
2019-01-22 23:05:23 +0100

    Wrapping up works on "our pets" page

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,51 @@
 <template>
-  <div>
-    <h1>Not only people need a house</h1>
-    <h2>Not only people need a house</h2>
-    <h3>Not only people need a house</h3>
-    <h4>Not only people need a house</h4>
-    <h5>Not only people need a house</h5>
-    <h6>Not only people need a house</h6>
-    <p>Some larger amount of text just to see how it's gona look...</p>
+  <div class="app-container">
+    <section class="hero is-fullheight">
+      <div class="hero-header">
+        <NavBar/>
+      </div>
+      <div class="hero-body">
+        <h3>Nie tylko ludzie potrzebują domu</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tincidunt, augue non finibus volutpat, risus ipsum tristique nulla, non interdum orci nunc eu velit. Nam eleifend augue arcu, eget eleifend eros scelerisque sed.</p>
+        <button class="button is-primary is-medium is-rounded">Adoptuj przyjaciela</button>
+      </div>
+      <div class="hero-foot"></div>
+    </section>
   </div>
 </template>
 
 <script>
+import NavBar from "./components/NavBar";
+
 export default {
   name: "app",
-  components: {}
+  components: { NavBar }
 };
 </script>
 
 <style lang="scss">
 @import "./scss/base.scss";
+
+.hero {
+  padding: 2rem 5rem;
+  background-image: url("./assets/dog-hero.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+.hero.is-fullheight .hero-body {
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: left;
+}
+.hero-body {
+  max-width: 45rem;
+  height: auto;
+}
+.her-body,
+h3,
+p,
+button {
+  margin: 1rem;
+}
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav
+    class="navbar navbar-container is-transparent"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div class="navbar-brand">
+      <img src="./../../public/logo.png">
+    </div>
+
+    <div class="navbar-menu is-active">
+      <div class="navbar-end">
+        <a class="navbar-item">O schronisku</a>
+        <a class="navbar-item is-active">Nasze zwierzaki</a>
+        <a class="navbar-item">Pomoc</a>
+        <a class="navbar-item">Kontakt</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+@import "./../scss/base.scss";
+.navbar-menu {
+  box-shadow: none;
+}
+.navbar-brand img {
+  width: 125px;
+  height: auto;
+}
+</style>
