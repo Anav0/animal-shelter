17016d08e6546b711e81087960daf4fb79e0bf77
Anav <janbul6@gmail.com>
2019-02-21 20:40:58 +0100

    Added google analytics via plugin
    Added google analytics via plugin

--- src/main.js
+++ src/main.js
@@ -19,9 +19,23 @@ import {
   faBars
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import VueAnalytics from "vue-analytics";
 
 Vue.config.productionTip = false;
 Vue.use(VuePaginate);
+const isProd = process.env.NODE_ENV === "production";
+Vue.use(VueAnalytics, {
+  id: "UA-134709304-1",
+  router,
+  checkDuplicatedScript: true,
+  autoTracking: {
+    screenview: true
+  },
+  debug: {
+    enabled: !isProd,
+    sendHitTask: isProd
+  }
+});
 library.add(
   faEnvelope,
   faPhone,
